789a5469ff21285ffd4b43932218c386faa0c42a
Jean-Marc Goepfert <goepfertjeanmarc@gmail.com>
2017-05-17 08:44:01 +1000

    Include all data, don't rename useage keys

--- src/parse_verbs/core.clj
+++ src/parse_verbs/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.data.json :as json]
             [clojure.set :refer [rename-keys]]
             [clojure.string :refer [lower-case]]
+            [cheshire.core :refer :all]
             ))
 
 (def verbs
@@ -14,48 +15,46 @@
                      "mood_english"
                      "tense"
                      "tense_english"
+                     "verb_english"
                      "form_1s"
                      "form_2s"
                      "form_3s"
                      "form_1p"
                      "form_2p"
                      "form_3p"
-                     ]]
+                     "gerund"
+                     "gerund_english"
+                     "pastparticiple"
+                     "pastparticiple_english"]]
     (map #(select-keys % wanted-keys) verbs)))
-
-(defn rename [verbs]
-  (map #(rename-keys %
-               {"form_1s" "Yo"
-                "form_2s" "Tú"
-                "form_3s" "El/Ella/Usted"
-                "form_1p" "Nosotr(o/a)s"
-                "form_2p" "Vosotr(o/a)s"
-                "form_3p" "Ell(o/a)s/Ustedes"}) verbs))
-
-(def expected
-  {"hablar" {:translation "to eat"
-             :useage {"" {:present {"yo" "hablo"
-                                             "tu" "hablas"}}
-                     :subjunctive {:present {"yo" "hable"
-                                             "tu" "hables"}}}}})
 
 (defn create-thing [verbs]
   (reduce
     (fn [acc v]
       (let [tense (lower-case (v "tense_english"))
             mood (lower-case (v "mood_english"))
-            rs (select-keys v ["Yo"
-                               "Tú"
-                               "El/Ella/Usted"
-                               "Nosotr(o/a)s"
-                               "Vosotr(o/a)s"
-                               "Ell(o/a)s/Ustedes"])]
-        (assoc-in acc [(v "infinitive") :useage mood tense] rs)))
+            rs (select-keys v ["form_1s"
+                               "form_2s"
+                               "form_3s"
+                               "form_1p"
+                               "form_2p"
+                               "form_3p"])
+            obj (assoc-in acc [(v "infinitive") :useage mood tense] rs)]
+        (if (obj "infinitive")
+          obj
+          (assoc-in obj  [(v "infinitive") :meta] (select-keys v ["infinitive"
+                                     "infinitive_english"
+                                     "verb_english"
+                                     "gerund"
+                                     "gerund_english"
+                                     "pastparticiple"
+                                     "pastparticiple_english"
+                                     ])))
+        ))
     {}
     verbs))
 
 (def a (strip-stuff verbs))
-(def b (rename a))
-(def c (create-thing b))
+(def c (create-thing verbs))
 
-(spit "resources/verbs2.json" (json/write-str c))
+(spit "resources/verbs2.json" (generate-string c {:pretty true}))
